5ac62b48dd8affa94f6ce61d46c4116ef530cae1
Cai Runpei <cairunpei@outlook.com>
2024-05-07 22:23:14 +0800

    Update v2config.js to clash cfg

--- v2config.js
+++ v2config.js
@@ -1,66 +1,53 @@
-export default vpnCookie => `{
-  "log": {
-    "loglevel": "warning"
-  },
-  "routing": {
-    "domainStrategy": "AsIs",
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "geoip:private"
-        ],
-        "outboundTag": "proxy"
-      }
-    ]
-  },
-  "inbounds": [
-    {
-      "listen": "127.0.0.1",
-      "port": "1080",
-      "protocol": "socks",
-      "settings": {
-        "auth": "noauth",
-        "udp": true,
-        "ip": "127.0.0.1"
-      }
-    },
-    {
-      "listen": "127.0.0.1",
-      "port": "1081",
-      "protocol": "http"
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "vmess",
-      "settings": {
-        "vnext": [
-          {
-            "address": "example-1234-p.vpn.ysu.edu.cn",
-            "port": 8118,
-            "users": [
-              {
-                "id": "example-user-id"
-              }
-            ]
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "headers": {
-            "Cookie": "${vpnCookie}"
-          }
-        }
-      },
-      "tag": "proxy"
-    },
-    {
-      "protocol": "freedom",
-      "tag": "direct"
-    }
-  ]
-}
+export default vpnCookie => `
+mode: rule
+mixed-port: 8001
+allow-lan: true
+log-level: warn
+secret: ''
+external-controller: 127.0.0.1:9090
+dns:
+  enable: true
+  listen: 127.0.0.1:5335
+  default-nameserver:
+  - 180.184.1.1
+  - 119.29.29.29
+  - 223.5.5.5
+  use-hosts: true
+  nameserver:
+  - https://223.6.6.6/dns-query
+  - https://120.53.53.53/dns-query
+  - tls://223.5.5.5:853
+  fallback:
+  - https://101.101.101.101/dns-query
+  - https://public.dns.iij.jp/dns-query
+  - https://208.67.220.220/dns-query
+  fake-ip-range: 198.18.0.1/16
+tun:
+  enable: true
+  stack: gvisor
+  dns-hijack:
+  - any:53
+  auto-route: true
+  auto-detect-interface: true
+proxies:
+- name: 503-Server
+  type: vmess
+  server: 10-20-75-221-1234-p.vpn.ysu.edu.cn
+  port: 8118
+  uuid: 69794b15-d079-1eec-0e87-e8e1806826ff
+  alterId: 0
+  cipher: auto
+  # udp: false
+  tls: true
+  network: ws
+  ws-opts:
+    headers:
+      Cookie: TWFID=${vpnCookie}
+proxy-groups:
+- name: YSU
+  type: select
+  proxies:
+  - 503-Server
+rules:
+- MATCH,YSU
 `;
